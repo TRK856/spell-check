0e05fb84fc8ecfeeadd4a16072b15cae02dbe881
TRK856 <53193704+TRK856@users.noreply.github.com>
2022-10-12 09:28:58 -0600

    DONE

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Text.RegularExpressions;
 using System.Diagnostics;
+#nullable disable
 
 class Program
 {
@@ -15,32 +16,32 @@ class Program
         String[] dictionary = System.IO.File.ReadAllLines(@"data-files/dictionary.txt");
         String aliceText = System.IO.File.ReadAllText(@"data-files/AliceInWonderLand.txt");
         String[] aliceWords = Regex.Split(aliceText, @"\s+");
+        Stopwatch timer = new Stopwatch();
 
         // Print first 50 values of each list to verify contents
         // actual program
-        bool loop = true;
-        while (loop)
+        while (true)
         {
+            Console.Clear();
             Console.WriteLine($"MAIN MENU");
             Console.WriteLine("    1. Spell Check a Word (Linear Search)");
             Console.WriteLine("    2. Spell Check a Word (Binary Search)");
             Console.WriteLine("    3. Spell Check Alice In Wonderland (Linear Search)");
             Console.WriteLine("    4. Spell Check Alice In Wonderland (Binary Search)");
             Console.WriteLine("    5. Exit");
-
             Console.Write("-> ");
             int mainMenuChoice = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
             if (mainMenuChoice == 1)
             {
-                Console.Clear();
                 Console.WriteLine("SPELL CHECK A WORD (LINEAR SEARCH)");
                 Console.Write("Please enter a word: ");
                 string sortWord = Console.ReadLine();
                 Console.Clear();
                 Console.WriteLine("Searching...");
-                Stopwatch timer = new Stopwatch();
                 timer.Start();
-                int pos = stringlinearSearch(dictionary, sortWord);
+                int pos = linearSearch(dictionary, sortWord);
                 timer.Stop();
                 TimeSpan timeElapsed = timer.Elapsed;
                 Console.Clear();
@@ -58,71 +59,94 @@ class Program
                 }
                 ;
             }
-                    if (mainMenuChoice == 2)
+            if (mainMenuChoice == 2)
+            {
+                Console.WriteLine("SPELL CHECK A WORD (BINARY SEARCH)");
+                string sortWord = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("Searching...");
+                timer.Start();
+                int pos = binarySearch(dictionary, sortWord);
+                timer.Stop();
+                TimeSpan timeElapsed = timer.Elapsed;
+                Console.Clear();
+                if (pos != -1)
+                {
+                    Console.WriteLine(
+                        $"'{sortWord}' is IN the dictionary at line #{pos}. ({timeElapsed} seconds)"
+                    );
+                }
+                else
+                {
+                    Console.WriteLine(
+                        $"'{sortWord}' is NOT in the dictionary. ({timeElapsed} seconds)"
+                    );
+                }
+                ;
+            }
+            if (mainMenuChoice == 3)
+            {
+                Console.WriteLine("SPELL CHECK A WORD (LINEAR SEARCH)");
+                Console.WriteLine("Checking...");
+                timer.Start();
+                int count = 0;
+                for (int i = 0; i < aliceWords.Length; i++)
+                {
+                    int pos = linearSearch(dictionary, aliceWords[i].ToLower());
+                    if (pos == -1)
                     {
-                        Console.Clear();
-                        Console.WriteLine("SPELL CHECK A WORD (BINARY SEARCH)");
-                        Console.Write("Please enter a word: ");
-                        string sortWord = Console.ReadLine();
-                        Console.Clear();
-                        Console.WriteLine("Searching...");
-                        Stopwatch timer = new Stopwatch();
-                        timer.Start();
-                        int pos = stringBinarySearch(dictionary, sortWord);
-                        timer.Stop();
-                        TimeSpan timeElapsed = timer.Elapsed;
-                        Console.Clear();
-                        if (pos != -1)
-                        {
-                            Console.WriteLine(
-                                $"'{sortWord}' is IN the dictionary at line #{pos}. ({timeElapsed} seconds)"
-                            );
-                        }
-                        else
-                        {
-                            Console.WriteLine(
-                                $"'{sortWord}' is NOT in the dictionary. ({timeElapsed} seconds)"
-                            );
-                        };
+                        count++;
                     }
-                    // if (mainMenuChoice == 3)
-                    // {
-                    //     Console.Clear();
-                    //     Console.WriteLine("ALL NICKNAMES");
-                    //     for (int i = 0; i < nickname.Count(); i++)
-                    //     {
-                    //         Console.WriteLine($"{firstName} '{nickname[i]}' {lastName}");
-                    //     }
-                    // }
-            //         if (mainMenuChoice == 4)
-            //         {
-            //             Console.Clear();
-            //             Console.WriteLine("ADD A NICKNAME");
-            //             Console.Write("Please enter a nickname to add: ");
-            //             string newNickname = Console.ReadLine();
-            //             bool found = false;
-            //             for (int i = 0; i < nickname.Count(); i++)
-            //             {
-            //                 if (nickname[i] == newNickname)
-            //                 {
-            //                     found = true;
-            //                 }
-            //             }
-            //             if (found == true)
-            //             {
-            //                 Console.WriteLine($"{newNickname} is already in the nickname list.");
-            //             }
-            //             else
-            //             {
-            //                 nickname.Add(newNickname);
-            //                 Console.WriteLine($"{newNickname} has been added to the nickname list.");
-            //             }
-            //         }
-            //         if (mainMenuChoice == 5)
-            //         {
-            //             Console.Clear();
-            //             break;
-            //         }
+                }
+                timer.Stop();
+                TimeSpan timeElapsed = timer.Elapsed;
+                Console.Clear();
+                if (count == 0)
+                {
+                    Console.WriteLine($"All words seem good! ({timeElapsed})");
+                }
+                else
+                {
+                    Console.WriteLine(
+                        $"Number of words not found in dictionary: {count} ({timeElapsed})"
+                    );
+                }
+                ;
+            }
+            if (mainMenuChoice == 4)
+            {
+                Console.WriteLine("SPELL CHECK A WORD (BINARY SEARCH)");
+                Console.WriteLine("Checking...");
+                timer.Start();
+                int count = 0;
+                for (int i = 0; i < aliceWords.Length; i++)
+                {
+                    int pos = binarySearch(dictionary, aliceWords[i].ToLower());
+                    if (pos == -1)
+                    {
+                        count++;
+                    }
+                }
+                timer.Stop();
+                TimeSpan timeElapsed = timer.Elapsed;
+                Console.Clear();
+                if (count == 0)
+                {
+                    Console.WriteLine($"All words seem good! ({timeElapsed})");
+                }
+                else
+                {
+                    Console.WriteLine(
+                        $"Number of words not found in dictionary: {count} ({timeElapsed})"
+                    );
+                }
+                ;
+            }
+            if (mainMenuChoice == 5)
+            {
+                Console.Clear();
+                break;
+            }
             Console.WriteLine("");
             Console.WriteLine("Back to Main Menu?");
             Console.Write("-> ");
@@ -142,7 +166,7 @@ class Program
     // Search an array for an item using the linear search algorithm.
     // If item found, return index where found, else return -1.
     // string linear search
-    public static int stringlinearSearch(string[] anArray, string item)
+    public static int linearSearch(string[] anArray, string item)
     {
         for (int i = 0; i < anArray.Length; i++)
         {
@@ -158,7 +182,7 @@ class Program
 
     // Search a SORTED array for an item using the binary search algorithm.
     // If item found, return index where found, else return -1.
-    public static int stringBinarySearch(string[] anArray, string item)
+    public static int binarySearch(string[] anArray, string item)
     {
         int lowerIndex = 0;
         int upperIndex = anArray.Length - 1;
